e36fec0b028024466cf1b94e275a64a894291606
stilianb <stilianb@gmail.com>
2023-12-05 09:40:12 -0700

    added argument parser

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import helpers as h
 import constants as c
 import cv2
+import argparse
 
 
 def main():
     video = cv2.VideoCapture(c.input_video_path)  # Video input
 
-    converted_frames = h.convertFrames(video)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--video', dest='v', action='store_true',
+                        help="create video from input folder")
 
-    print("Converting Frames into Gif")
-    h.createGif(converted_frames)
+    args = parser.parse_args()
 
-    print("Done!")
+    if args.v:
+        print("Converting original video to ascii...")
+        converted_frames = h.convertFrames(video)
+
+        print("Converting Frames into Gif...")
+        h.createGif(converted_frames)
+
+        print("Done!")
 
     video.release()
     cv2.destroyAllWindows()
