a01ddd7bd413eb9b8e60a2a493a97cc7ed8a6ece
stilianb <stilianb@gmail.com>
2023-12-10 16:47:28 -0700

    fixed image dimensions in output for 1920x1080

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-frame_columns = 90
+frame_columns = 91
 frame_scale = 1
 
 gscale = ' @%#*+=-_:;.\'1234567890abcdefghijklmnopqrstuvwxyz'
@@ -11,7 +11,7 @@ scaffold_h = 1080
 
 white_bg = (255, 255, 255)
 
-key_variance_x = 20
-key_variance_y = 20
+key_variance_x = 21.1
+key_variance_y = 21
 
 scale_err = "Image too small for specified cols!"
